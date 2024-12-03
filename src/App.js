291846ffcade7039af2e712fccde388b1603dfc9
Luke <propertygroup@gmx.com>
2024-12-03 12:55:09 +0000

    remove home page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Login from './components/Login.js';
 import OAuthCallback from './components/OAuthCallback.js';
 import PrivateRoute from './components/PrivateRoute.js';
-import Home from './components/Home.js';
 import Searches from './components/Searches.js';
 import Alerts from './components/Alerts.js';
 import Settings from './components/Settings.js';
@@ -13,7 +12,7 @@ import NotSub from './components/NotSub.js';
 
 const DashboardLayout = ({ children, pageTitle }) => (
   <div>
-    <Navigation /> {/* Move this here for testing */}
+    <Navigation />
     <Header pageTitle={pageTitle} />
     <div className="content">
       {children}
@@ -25,12 +24,12 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to={localStorage.getItem('token') ? "/dashboard" : "/login"} />} />
+        <Route path="/" element={<Navigate to={localStorage.getItem('token') ? "/dashboard/searches" : "/login"} />} />
         <Route path="/login" element={<Login />} />
         <Route path="/oauth/callback" element={<OAuthCallback />} />
         <Route path="/notsub" element={<NotSub />} />
         <Route element={<PrivateRoute />}>
-          <Route path="/dashboard" element={<DashboardLayout pageTitle="Dashboard"><Home /></DashboardLayout>} />
+          <Route path="/dashboard" element={<Navigate to="/dashboard/searches" />} />
           <Route path="/dashboard/searches" element={<DashboardLayout pageTitle="Searches"><Searches /></DashboardLayout>} />
           <Route path="/dashboard/alerts" element={<DashboardLayout pageTitle="Alerts"><Alerts /></DashboardLayout>} />
           <Route path="/dashboard/settings" element={<DashboardLayout pageTitle="Settings"><Settings /></DashboardLayout>} />
